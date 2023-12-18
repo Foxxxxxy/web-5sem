d79f5d6f6abd17969c16cbddccfd884544e4bc1d
Nikita Khaurov <ikhaurovnikita@gmail.com>
2023-12-18 23:52:57 +0300

    fix

--- js/LoadAbout.js
+++ js/LoadAbout.js
@@ -7,15 +7,15 @@ function loadAbout() {
 </ul>
 <p class="list__title">My skills</p>
 <ul class="list">
-  <li class="list__item"><img class="list__logo" src="icons/skills/java.svg">Java</li>
-  <li class="list__item"><img class="list__logo" src="icons/skills/kotlin.svg">Kotlin</li>
-  <li class="list__item"><img class="list__logo" src="icons/skills/python.svg">Python</li>
+  <li class="list__item"><img class="list__logo" src="./icons/skills/java-svgrepo-com.svg">Java</li>
+  <li class="list__item"><img class="list__logo" src="./icons/skills/kotlin-svgrepo-com.svg">Kotlin</li>
+  <li class="list__item"><img class="list__logo" src="./icons/skills/python.svg">Python</li>
   <ul class="list">
     <li class="list__item">bs4</li>
     <li class="list__item">aiogram</li>
     <li class="list__item">requests</li>
     <li class="list__item">FASTApi</li>
   </ul>
-  <li class="list__item"><img class="list__logo" src="icons/skills/go.svg">Go</li>
+  <li class="list__item"><img class="list__logo" src="./icons/skills/go-svgrepo-com.svg">Go</li>
 </ul>`
 }
